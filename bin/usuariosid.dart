809fcaded6b574814c100e16258642787673d38c
Thania <tjyaimacubillos@gmail.com>
2026-06-10 09:02:49 -0500

    version 3.0

--- bin/usuariosid.dart
+++ bin/usuariosid.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:usuariosid/buscar.dart';
+import 'package:usuariosid/listar.dart';
+
 void main (List<String>arguments){
   List <Map <String,dynamic >> usuarios =[
    {"id":01,"nombre": "Cristian","celular":"1234567890"},
@@ -7,7 +12,10 @@ void main (List<String>arguments){
    {"id":05,"nombre": "Mariana","celular":"1234567894"},
    {"id":06,"nombre": "Karol","celular":"1234567895"}
 ];
-
+ listarUsuario(usuarios);
+  stdout.writeln("Buscar nombre");
+  String nombre= stdin.readLineSync()??"";
+  buscarUsuarios(usuarios,nombre);
 }
 
  
--- lib/listar.dart
+++ lib/listar.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 void listarUsuario(List <Map <String,dynamic >> usuarios){
   for(var usuario in usuarios){
     print("id: ${usuario["id"]}");
     print("nombre: ${usuario["nombre"]}");
     print("celular: ${usuario["celular"]}");
   }
+ 
 }
